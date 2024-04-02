086973ad838f785e37416338bcafa0b1d6c507f5
And-ers <accornect@mun.ca>
2024-04-02 13:33:00 -0230

    modified:   src/indevendent.py

--- src/indevendent.py
+++ src/indevendent.py
@@ -1,39 +1,59 @@
 import sys
 import PyQt6.QtWidgets as widgets
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFont, QIntValidator
 from PyQt6.QtCore import Qt
+
+CATEGORY_WIDTH = 200
+PRICE_WIDTH = 50
+AMOUNT_WIDTH = 50
+SELL_COUNT_WIDTH = 50
 
 class invItemWidget(widgets.QWidget):
 
     categories = ['-']
+    deals = {
+        '-' : None
+        }
 
     @classmethod
     def add_category(self, name):
         invItemWidget.categories.append(name)
         invItemWidget.categories.sort()
+        invItemWidget.deals.update({name : None})
 
-    def __init__(self):
+    def __init__(self, name = 'New Product', category = '-', price = 0.0, count = 0, parent_window = None):
         super(invItemWidget, self).__init__()
-
-        self.product_name = 'New Product'
-        self.product_category = '-'
-        self.price = 0.0
-        self.inv_count = 0
+        self.product_name = name
+        self.product_category = category
+        self.price = price
+        self.inv_count = count
+        self.parent_window = parent_window
 
         self.name_box = widgets.QLineEdit(self.product_name)
         self.category_box = widgets.QComboBox()
         self.category_box.addItems(invItemWidget.categories)
-        self.category_box.setFixedWidth(100)
+        self.category_box.setFixedWidth(CATEGORY_WIDTH)
         self.price_box = widgets.QLineEdit(str(self.price))
-        self.price_box.setFixedWidth(50)
+        self.price_box.setFixedWidth(PRICE_WIDTH)
+        self.amountBox = widgets.QSpinBox()
+        self.amountBox.setButtonSymbols(widgets.QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.amountBox.setRange(0,999)
+        self.amountBox.setFixedWidth(AMOUNT_WIDTH)
+        self.sellCountBox = widgets.QSpinBox()
+        self.sellCountBox.setButtonSymbols(widgets.QAbstractSpinBox.ButtonSymbols.PlusMinus)
+        self.sellCountBox.setRange(0,self.inv_count)
+        self.sellCountBox.setFixedWidth(SELL_COUNT_WIDTH)
 
+        self.amountBox.editingFinished.connect(self.updateAmount)
         self.name_box.editingFinished.connect(self.setName)
         self.price_box.editingFinished.connect(self.setPrice)
 
         self.hbox = widgets.QHBoxLayout()
         self.hbox.addWidget(self.name_box)
         self.hbox.addWidget(self.category_box)
+        self.hbox.addWidget(self.amountBox)
         self.hbox.addWidget(self.price_box)
+        self.hbox.addWidget(self.sellCountBox)
 
         self.setLayout(self.hbox)
 
@@ -50,16 +70,34 @@ class invItemWidget(widgets.QWidget):
     def hide(self):
         for w in [self, self.name_box, self.category_box, self.price_box]:
             w.setVisible(False)
+    
+    def updateAmount(self):
+        self.inv_count = self.amountBox.value()
+        self.sellCountBox.setRange(0,self.inv_count)
+
+    def complete_sale(self):
+        number_sold = self.sellCountBox.value()
+        new_amount = self.amountBox.value() - number_sold
+        self.sellCountBox.setValue(0)
+        self.amountBox.setValue(new_amount)
+        self.updateAmount()
+        sale = {
+            'category': self.product_category,
+            'price': self.price,
+        }
+        return sale
 
 
 class MainWindow(widgets.QMainWindow):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.setWindowTitle('PyQt Inventory Management System')
-        self.setGeometry(100, 100, 600, 500)
+        self.setGeometry(100, 100, 900, 500)
 
         self.items = []
+        self.total_profit = 0.0
 
         self.itemPanel = widgets.QWidget()
         self.itemPanelLayout = widgets.QVBoxLayout()
@@ -84,6 +122,14 @@ class MainWindow(widgets.QMainWindow):
         self.addingMenuLayout.addWidget(self.addCategoryBox)
         self.addingMenuLayout.addWidget(self.addCategoryButton)
 
+        self.addingMenuLayout.addSpacerItem(widgets.QSpacerItem(1,1,widgets.QSizePolicy.Policy.Minimum, widgets.QSizePolicy.Policy.Expanding))
+        self.profitLabel = widgets.QLabel("Today's Profit:   $ --.--")
+        self.addingMenuLayout.addWidget(self.profitLabel)
+
+        self.sellButton = widgets.QPushButton('Sell')
+        self.sellButton.clicked.connect(self.sale_update_inventory)
+        self.addingMenuLayout.addWidget(self.sellButton)
+
         self.addingMenu.setLayout(self.addingMenuLayout)
         self.addingDock.setWidget(self.addingMenu)
 
@@ -102,11 +148,38 @@ class MainWindow(widgets.QMainWindow):
         self.scroller.setWidget(self.itemPanel)
 
         self.searchbar = widgets.QLineEdit()
+        self.searchbar.setPlaceholderText('Search items...')
         self.searchbar.textChanged.connect(self.update_display)
+
+        headers = widgets.QWidget()
+        headersLayout = widgets.QHBoxLayout()
+        headersLayout.addSpacerItem(widgets.QSpacerItem(40,1))
+        PROD_LABEL = widgets.QLabel('Product')
+        PROD_LABEL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        headersLayout.addWidget(PROD_LABEL)
+        CAT_LABEL = widgets.QLabel('Category')
+        CAT_LABEL.setFixedWidth(CATEGORY_WIDTH)
+        CAT_LABEL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        headersLayout.addWidget(CAT_LABEL)
+        AMT_LABEL = widgets.QLabel('In Stock')
+        AMT_LABEL.setFixedWidth(AMOUNT_WIDTH)
+        AMT_LABEL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        headersLayout.addWidget(AMT_LABEL)
+        PRC_LABEL = widgets.QLabel('Price')
+        PRC_LABEL.setFixedWidth(PRICE_WIDTH)
+        PRC_LABEL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        headersLayout.addWidget(PRC_LABEL)
+        SELL_LABEL = widgets.QLabel('Sell')
+        SELL_LABEL.setFixedWidth(SELL_COUNT_WIDTH)
+        SELL_LABEL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        headersLayout.addWidget(SELL_LABEL)
+        headersLayout.addSpacerItem(widgets.QSpacerItem(30,1))
+        headers.setLayout(headersLayout)
 
         container = widgets.QWidget()
         containerLayout = widgets.QVBoxLayout()
         containerLayout.addWidget(self.searchbar)
+        containerLayout.addWidget(headers)
         containerLayout.addWidget(self.scroller)
 
         container.setLayout(containerLayout)
@@ -123,17 +196,29 @@ class MainWindow(widgets.QMainWindow):
                 item.hide()
 
     def add_blank_item(self):
-        new_item = invItemWidget()
+        new_item = invItemWidget(parent_window = self)
         self.items.append(new_item)
         self.itemPanelLayout.insertWidget(self.itemPanelLayout.count()-1, new_item)
         self.itemPanel.setLayout(self.itemPanelLayout)
 
     def add_new_category(self):
         new_category = self.addCategoryBox.text()
-        if new_category != '':
+        if new_category != '' and new_category not in invItemWidget.categories:
             invItemWidget.add_category(new_category)
             for item in self.items:
                 item.category_box.addItem(new_category)
+        self.addCategoryBox.clear()
+
+    def sale_update_inventory(self):
+        sales = []
+        for item in self.items:
+            sales.append(item.complete_sale())
+        sale_amount = sum([sale['price'] for sale in sales])
+        self.increase_profit(sale_amount)
+
+    def increase_profit(self, amount):
+        self.total_profit += amount
+        self.profitLabel.setText(f"Today's Profit: ${self.total_profit:.2f}")
 
 if __name__ == '__main__':
     app = widgets.QApplication(sys.argv)
